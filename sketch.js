55e1c33bcc0e30622af06d5d2ed1e6e3d1ded493
soysai <145474777+soysai@users.noreply.github.com>
2024-04-25 22:02:44 -0400

    update

---
+++ sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(1080, 1920);
+  background(0);
+  noStroke();
+ 
+}
+
+function draw() {
+  background(0);
+  if (mouseIsPressed) {
+  
+    if (mouseX, mouseY >=50){
+    fill(225, 0, 0,);
+    ellipse(mouseX, mouseY, 50, 50);
+    
+    fill(0, 15);
+    rect(0, 0, width, height);
+      
+    }
+    
+     if (mouseX,mouseY <= 150){
+      fill('#fae')
+      ellipse(mouseX, mouseY, 50, 50);
+       fill(0, 15);
+    rect(0, 0, width, height);
+    }
+    
+    if (mouseX, mouseY >= 300){
+       fill('rgb(0,255,0)')
+      ellipse(mouseX, mouseY, 50, 50);
+       fill(0, 15);
+    rect(0, 0, width, height);
+    }
+     if (mouseX <= 200){
+       fill(color(0, 0, 255))
+      ellipse(mouseX, mouseY, 50, 50);
+       fill(0, 15);
+    rect(0, 0, width, height);
+    }
+    
+    
+    if (mouseIsPressed === true){
+      ellipse(mouseX,mouseY, 50, 50);
+    }
+  }
+  }
+}
